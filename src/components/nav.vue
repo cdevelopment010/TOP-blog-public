f4aae68db0a63cb68dc04f9da71d36eade0a5333
Craig <cdevelopment010@gmail.com>
2025-01-05 19:52:25 +0000

    Feat: add navbar, and card components

---
+++ src/components/nav.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="d-flex">
+        <div>
+            <h2>Coffee Shop Coding</h2>
+        </div>
+
+        <div>
+            <RouterLink to="/posts" class="me-2">Posts</RouterLink>
+            <RouterLink to="/tags">Tags</RouterLink>
+        </div>
+
+    </div>
+
+</template>
+
+<script setup lang="ts">
+    import { RouterLink } from 'vue-router';
+</script>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,15 @@
 <template>
-    <h1>HOME</h1>
+    <NavComponent />
+    
+    <div class="container">
+        Top 5 posts here in a grid fashion  
+        <p>Post 1 will span full width</p>
+        <p>-- P1 P1 --</p>
+        <p>-- P2 P3 --</p>
+        <p>-- P4 P5 --</p>
+    </div>
 </template>
 
 <script setup lang="ts">
+    import NavComponent from '../components/nav.vue'
 </script>
