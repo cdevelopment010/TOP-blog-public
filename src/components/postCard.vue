f6e01df95d8896c83dc4ac6a0acbf2b4aa19396d
cdevelopment010 <cdevelopment010@gmail.com>
2025-01-08 21:32:43 +0000

    Fix: styling for postCard

--- src/components/postCard.vue
+++ src/components/postCard.vue
@@ -1,18 +1,27 @@
 <template>
     <RouterLink :to="`/post/${props.post.slugCombined}`">
         <div class="card">
-            <div class="card-header">{{ props.post.title }}</div>
+            <div class="card-header"><h3>{{ props.post.title }}</h3></div>
             <!-- <div class="card-body">
                 
             </div> -->
+            <div class="card-tags d-flex gap-1">
+                <template v-for="tag in tags">
+                    <RouterLink :to="`/posts/publishedPosts/tags/${tag}`" class="pill">{{ tag }}</RouterLink>
+                </template>
+            </div>
 
-            
+
         </div>
     </RouterLink>
 </template>
 
 <script setup lang="ts">
-
+    import { ref } from "vue";
+    const tags = ref<string[]>([
+        'JS', 
+        'CSS'
+    ])
     const props = defineProps<{ post: any }>(); 
     //console.log(props.post.exerpt)
 
@@ -22,14 +31,15 @@
 .card { 
     padding: 1rem; 
     margin-bottom: 1rem;
-    border: 1px solid var(--text-800); 
-    border-radius: 5px;
+    border: 1px solid var(--text-200); 
+    border-radius: 12px;
 }
 
 
 .card:hover { 
-    box-shadow: 4px 4px 4px 0 var(--text-600);
-    border-color:var(--text-600) ;
+    box-shadow: 0px 3px 20px 0 var(--text-200);
+    border-color:var(--text-200) ;
+    transform: translateY(-1px);
 }
 .card-header { 
     color: var(--text-800)
