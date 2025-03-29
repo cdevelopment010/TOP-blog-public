e836a85aaaafd5a4e9fea07ff820506e78c3e6ce
Craig <cdevelopment010@gmail.com>
2025-03-29 09:05:51 +0000

    Feat: Update about page with social links

--- src/views/about.vue
+++ src/views/about.vue
@@ -17,8 +17,16 @@
                 This blog started off as a project from <a href="https://www.theodinproject.com/lessons/node-path-nodejs-blog-api" target="_blank">TOP</a>, but I wanted to learn more about domain management and analytics.
             </p>
             <p>
-                I thought why not write about code and tech to learn even more? Keep snippets that I have found useful, and maybe others might find useful!
+                I thought, why not write about code and tech to learn even more? Keep snippets that I have found useful, and maybe others might find useful!
             </p>
+
+            <h3>Socials</h3>
+            <p>Below is a list of socials if you want to follow me other places. YouTube and Twitch haven't been uploaded to for a while, but will start them up again soon!</p>
+            <div class="d-flex gap-1">
+                <a href="https://github.com/cdevelopment010/" target="_blank" class="link-none link-github" title="GitHub"><i class="fa-brands fa-github fa-3x"></i></a>
+                <a href="https://www.youtube.com/@CraigDavisonDev" target="_blank"  class="link-none link-youtube" title="YouTube"><i class="fa-brands fa-youtube fa-3x"></i></a>
+                <a href="https://www.twitch.tv/cdev010" target="_blank"  class="link-none link-twitch" title="Twitch"><i class="fa-brands fa-twitch fa-3x"></i></a>
+            </div>
 
         </div>
     </div>
